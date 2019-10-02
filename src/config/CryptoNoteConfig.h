e4beee777cdb38491e67d6df7ae2c7f85ba9716a
Learner <linhdqtg@gmail.com>
2019-10-03 05:31:31 +0700

    Add level base size
    From wrkzcoin - Todo

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -339,10 +339,11 @@ namespace CryptoNote
     const size_t P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT = 5000; // 5 seconds
     const char P2P_STAT_TRUSTED_PUB_KEY[] = "";
 
-    const uint64_t DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE = 256; // 512 MB
+    const uint64_t DATABASE_WRITE_BUFFER_MB_DEFAULT_SIZE = 256; // 256 MB
     const uint64_t DATABASE_READ_BUFFER_MB_DEFAULT_SIZE = 64; // 64 MB
     const uint32_t DATABASE_DEFAULT_MAX_OPEN_FILES = 50; // 500 files
     const uint16_t DATABASE_DEFAULT_BACKGROUND_THREADS_COUNT = 4; // 4 DB threads
+    const uint64_t DATABASE_MAX_BYTES_FOR_LEVEL_BASE = 512; // Todo in MB
 
     const char LATEST_VERSION_URL[] = "https://github.com/bitcoinmono/bitcoinmono/releases";
 
