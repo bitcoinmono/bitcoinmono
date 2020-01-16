cceedb4ec98da5bac6d19e20217c3ede3e99c7c1
Learner <linhdqtg@gmail.com>
2020-01-16 08:41:27 +0700

    Log

--- src/cryptonotecore/TransactionPool.cpp
+++ src/cryptonotecore/TransactionPool.cpp
@@ -219,7 +219,7 @@ namespace CryptoNote
     bool TransactionPool::checkIfTransactionPresent(const Crypto::Hash &hash) const
     {
         if (transactionHashIndex.find(hash) == transactionHashIndex.end() && transactionHashIndex.size() > 1) {
-            logger(Logging::INFO) << "Found at " << transactionHashIndex.find(hash) << " in pool of total txes : " << transactionHashIndex.size();
+            logger(Logging::INFO) << "Found in pool of total txes : " << transactionHashIndex.size();
         }
         return transactionHashIndex.find(hash) != transactionHashIndex.end();
     }
