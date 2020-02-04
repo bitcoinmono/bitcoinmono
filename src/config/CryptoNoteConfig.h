577c109a81d5c745452c00e4f0f55c5fb58c80b2
Learner <linhdqtg@gmail.com>
2020-02-04 17:22:05 +0700

    2.0.8.15 Release
    Official release

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -348,9 +348,9 @@ namespace CryptoNote
 
     // P2P Network Configuration Section - This defines our current P2P network version
     // and the minimum version for communication between nodes
-    const uint8_t P2P_CURRENT_VERSION = 2;
+    const uint8_t P2P_CURRENT_VERSION = 3;
 
-    const uint8_t P2P_MINIMUM_VERSION = 0;
+    const uint8_t P2P_MINIMUM_VERSION = 1;
 
     // This defines the minimum P2P version required for lite blocks propogation
     const uint8_t P2P_LITE_BLOCKS_PROPOGATION_VERSION = 0;
